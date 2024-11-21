6bd364c5dafa28de401f763d3af22bbf3d37cff1
Victor Arruda <albq.victor@gmail.com>
2024-11-21 19:20:59 -0300

    feat: add navbar and card in blog

---
+++ blog.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-BR" data-bs-theme="dark">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PipelineBlog | Blog</title>
+</head>
+
+<body>
+  <?php include("navbar.php") ?>
+
+  <div class="container w-100">
+    <section class="d-flex">
+      <main class="p-3">
+        <div class="card w-100">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </main>
+      <aside></aside>
+    </section>
+  </div>
+</body>
+
+</html>
